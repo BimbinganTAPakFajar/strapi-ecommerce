7e5e2ca52e3bd711f8e3ede11000bbaade7b99ac
Melanchthon Butarbutar <Kyu-u>
2023-06-15 22:59:03 +0700

    add alamat, afterupdate

--- src/api/order/content-types/order/lifecycles.js
+++ src/api/order/content-types/order/lifecycles.js
@@ -1,9 +1,57 @@
 const { v4: uuidv4 } = require("uuid");
-
+const getTotalAmount = (cart) => {
+  const totalAmount = {};
+  cart.forEach((item) => {
+    const itemId = item.id;
+    const itemAmount = item.amount;
+    if (itemId in totalAmount) {
+      totalAmount[itemId] += itemAmount;
+    } else {
+      totalAmount[itemId] = itemAmount;
+    }
+  });
+  return totalAmount;
+};
 module.exports = {
   beforeCreate(event) {
     const { data } = event.params;
     console.log(data.uuid);
     data.uuid = uuidv4();
+  },
+  async afterUpdate(event) {
+    const { data } = event.params;
+    if (data.status === "Pesanan Diproses") {
+      const amounts = getTotalAmount(data.cart);
+      Object.keys(amounts).forEach(async (itemId) => {
+        const product = await strapi.entityService.findOne(
+          "api::product.product",
+          itemId,
+          {
+            populate: "*",
+          }
+        );
+        const newProduct = await strapi.entityService.update(
+          "api::product.product",
+          itemId,
+          {
+            data: {
+              stock: product.stock - amounts[itemId],
+            },
+          }
+        );
+        const productDetail = product.product_detail;
+        const itemAmount = amounts[itemId];
+        const sold = productDetail.sold + itemAmount;
+        const newProductDetail = await strapi.entityService.update(
+          "api::product-detail.product-detail",
+          productDetail.id,
+          {
+            data: {
+              sold: sold,
+            },
+          }
+        );
+      });
+    }
   },
 };
